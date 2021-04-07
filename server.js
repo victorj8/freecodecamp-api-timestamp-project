49829e17b02f9957e365c4cd0c77bf19c321557b
Victor Camacho <victor.camacho@block.one>
2021-04-07 14:13:52 -0400

    add timestamp endpoint

--- server.js
+++ server.js
@@ -18,13 +18,40 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
 
+app.get("/api/timestamp/:date?", function(req, res){
+  var date = new Date();
+
+  if(isNumeric(req.params.date)) {
+    date.setTime(req.params.date);
+  }else{
+    date = req.params.date === undefined ? new Date() : new Date(req.params.date);
+  }
+
+  if ( Object.prototype.toString.call(date) === "[object Date]" ) {
+    // it is a date
+    if ( isNaN( date.getTime() ) ) {  // d.valueOf() could also work
+      // date is not valid
+      res.json({error: "Invalid Date"});
+    }
+    else {
+      // date is valid
+      res.json({unix:date.valueOf(), utc: date.toGMTString()});
+    }
+  }else{
+    res.json({error: "Invalid Date"});
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
